b408f1f77160374a4316e9db541431a098daaf8e
horoiwa <horoiwa195@gmail.com>
2019-12-28 23:40:17 +0900

    Add GA test

--- tests/test_optiga.py
+++ tests/test_optiga.py
@@ -2,6 +2,7 @@ from logging import DEBUG, INFO, Formatter, StreamHandler, getLogger
 import pickle
 
 import matplotlib.pyplot as plt
+import pytest
 
 from optiga import __version__
 from optiga.optimizer import Optimizer
@@ -13,74 +14,60 @@ def test_version():
     assert __version__ == '0.1.0'
 
 
-def main():
-    """細かくndarrayの挙動をチェックしつつ
-    """
-    logger = getLogger(__name__)
-    logger.setLevel(DEBUG)
-    stream_handler = StreamHandler()
-    handler_format = Formatter(
-        '(%(levelname)s)[%(asctime)s]\n%(message)s')
-    stream_handler.setFormatter(handler_format)
-    logger.addHandler(stream_handler)
+class TestOptiga:
 
-    logger.info("Start Main test")
+    def setup_method(self):
 
-    pop_size = 500
-    length = 20
+        length = 20
+        init_popualtion = get_onemax_samples(500, length)
+        optimizer = Optimizer(samples=init_popualtion)
 
-    init_popualtion = get_onemax_samples(pop_size, length)
+        model1 = get_onemax_model()
+        optimizer.add_objective("ones", model1, direction="maximize")
 
-    optimizer = Optimizer(samples=init_popualtion)
+        model2 = get_linear_model(length)
+        optimizer.add_objective("linear_min", model2, direction="minimize")
 
-    model1 = get_onemax_model()
-    optimizer.add_objective("ones", model1, direction="maximize")
-
-    model2 = get_linear_model(length)
-    optimizer.add_objective("linear_min", model2, direction="minimize")
-
-    if False:
         model3 = get_linear_model(length)
         optimizer.add_objective("linear_max", model3, direction="maximize")
 
-    optimizer.add_discrete_constraint(fname="2", constraints=[0, 1, 2])
-    optimizer.add_discrete_constraint(fname="3", constraints=[0, 1, 2])
+        optimizer.add_discrete_constraint(fname="2", constraints=[0, 1, 2])
+        optimizer.add_discrete_constraint(fname="3", constraints=[0, 1, 2])
 
-    optimizer.add_valuerange_constraint(fname="2", lower=0, upper=2)
-    optimizer.add_valuerange_constraint(fname="3", lower=0, upper=2)
+        optimizer.add_valuerange_constraint(fname="2", lower=0, upper=2)
+        optimizer.add_valuerange_constraint(fname="3", lower=0, upper=2)
 
-    optimizer.add_onehot_groupconstraint(group=["4", "5"])
-    optimizer.add_onehot_groupconstraint(group=["6", "7"])
+        optimizer.add_onehot_groupconstraint(group=["4", "5"])
+        optimizer.add_onehot_groupconstraint(group=["6", "7"])
 
-    optimizer.add_sumtotal_groupconstraint(group=["8", "9", "10"],
-                                           lower=0, upper=2)
-    optimizer.add_sumtotal_groupconstraint(group=["11", "12"],
-                                           lower=0, upper=5)
+        optimizer.add_sumtotal_groupconstraint(group=["8", "9", "10"],
+                                               lower=1, upper=1)
+        optimizer.add_sumtotal_groupconstraint(group=["11", "12"],
+                                               lower=0, upper=1)
 
-    def test_func(X):
-        print("test func is called!!")
-        print(X.columns)
-        return X
+        def user_func(X):
+            print("test func is called!!")
+            print(X.columns)
+            return X
 
-    #optimizer.add_user_constraint(test_func)
+        optimizer.add_user_constraint(user_func)
 
-    optimizer.run(population_size=pop_size, n_gen=1)
+        self.optimizer = optimizer
 
-    Y = optimizer.pareto_front["Y"]
-    X = optimizer.pareto_front["X"]
+    def teardown_method(self):
+        del self.optimizer
 
-    if False:
-        print("Result")
-        sample_Y = optimizer.pareto_front["sample_Y"]
-        print(Y.shape)
-        plt.scatter(Y['ones'], Y['linear_min'])
-        plt.scatter(sample_Y['ones'], sample_Y['linear_min'])
-        plt.show()
+    def test_GA(self):
+        self.optimizer.run(population_size=300, n_gen=10)
 
-        X.to_csv("example/X.csv", index=False)
-        Y.to_csv("example/y.csv", index=False)
-
-    print(X.iloc[:, 5:10])
-    #print("Model1 MAX:", length)
-    #print("Model2 MIN:", model2.get_min_value())
-    #print("Model3 MAX:", model3.get_max_value())
+        Y = self.optimizer.pareto_front["Y"]
+        X = self.optimizer.pareto_front["X"]
+        for i in range(X.shape[0]):
+            row = X.iloc[i, :]
+            assert row["2"] in [0, 1, 2]
+            assert row["3"] in [0, 1, 2]
+            assert row[["4", "5"]].sum() == pytest.approx(1.0, 0.01)
+            assert row[["6", "7"]].sum() == pytest.approx(1.0, 0.01)
+            assert row[["8", "9", "10"]].sum() == pytest.approx(1.0, 0.01)
+            assert row[["11", "12"]].sum() <= 1.0 + 0.01
+            assert row[["11", "12"]].sum() >= 0.01
